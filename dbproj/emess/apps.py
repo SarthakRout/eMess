604a7f8ebf2117f8f260dea7c03c9bee381aac98
SarthakRout <sarthak2024@gmail.com>
2022-03-20 03:31:45 +0530

    Initialising database and setting up django

---
+++ dbproj/emess/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EmessConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'emess'
---
+++ dbproj/emess/models.py
@@ -0,0 +1,104 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
+from django.forms import ValidationError
+# Create your models here.
+
+class Student(User):
+    roll = models.IntegerField(
+        unique=True, 
+        blank=False,
+        primary_key=True
+    ) 
+    parent_hall = models.IntegerField(
+        unique=True,
+    )
+
+
+def uniq_ar(value):
+    checker = {}
+    for elem in value:
+        if elem in checker:
+            raise ValidationError(
+                ('%(elem)s is repeated more than once'), 
+                params={'elem':elem}
+            )
+        else:
+            checker[elem] = 'Present'
+
+class MessAdmin(User):
+    mobile_no = models.CharField(
+        max_length=15, 
+        blank=False
+    )
+    halls = ArrayField(
+        models.IntegerField(
+            blank=False
+        ), 
+        blank=False, 
+        max_length=15,
+        validators=[uniq_ar]
+    )
+
+class Meal(models.Model):
+    hall = models.IntegerField(
+        blank=False
+    )
+    meal_date = models.DateField(
+        blank=False
+    )
+    meal_start_time = models.TimeField(
+        blank=False
+    )
+    meal_end_time = models.TimeField(
+        blank=False
+    )
+    meal_type = models.CharField(
+        max_length=1, 
+        blank=False
+    )
+
+class ExtraItem(models.Model):
+    meal = models.ForeignKey(
+        Meal, 
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(
+        max_length=30, 
+        blank=False
+    )
+    price = models.IntegerField(
+        blank=False
+    )
+    max_amount = models.IntegerField(
+        blank=False
+    )
+    limit = models.IntegerField(
+
+    )
+
+class Order(models.Model):
+    roll = models.ForeignKey(
+        Student, 
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    extra_item = models.ForeignKey(
+        ExtraItem,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    quantity = models.IntegerField(
+        blank=False
+    )
+    booking_price = models.IntegerField(
+        blank=False
+    )
+    booked_at = models.DateTimeField(
+        blank=False,
+        auto_now_add=True
+    )
+    last_modified_at = models.DateTimeField(
+        blank=False,
+        auto_now=True
+    )
