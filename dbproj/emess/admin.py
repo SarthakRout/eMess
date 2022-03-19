8ca5bbd248cf3edc56d79e5d2b60c7bd2847abae
SarthakRout <sarthak2024@gmail.com>
2022-03-20 03:42:24 +0530

    Registered mode fields with admin

--- dbproj/emess/admin.py
+++ dbproj/emess/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ExtraItem, Meal, MessAdmin, Order, Student
+
+admin.site.register(Student)
+admin.site.register(MessAdmin)
+admin.site.register(Meal)
+admin.site.register(ExtraItem)
+admin.site.register(Order)
