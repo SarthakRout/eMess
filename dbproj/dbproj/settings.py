f710192bb7637b02ee7fbf9dd2692f83ae8b745f
SarthakRout <sarthak2024@gmail.com>
2022-04-12 18:26:08 +0530

    Remove django secret key

--- dbproj/dbproj/settings.py
+++ dbproj/dbproj/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_l^5sgw(d^fa%#rwz^frsx_43q8#w@5fvs=2o*odxvmrw@_^gi'
+# SECRET_KEY = 'django-insecure-_l^5sgw(d^fa%#rwz^frsx_43q8#w@5fvs=2o*odxvmrw@_^gi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
