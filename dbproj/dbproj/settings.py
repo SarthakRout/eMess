f6e2fc51642328b915fbf6ec661757dce70fc8d1
SarthakRout <sarthak2024@gmail.com>
2022-04-12 18:57:30 +0530

    g

--- dbproj/dbproj/settings.py
+++ dbproj/dbproj/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from corsheaders.defaults import default_headers
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,7 +90,8 @@ DATABASES = {
         'PORT': 5432
     }
 }
-
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
